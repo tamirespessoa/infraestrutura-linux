3f0acd0a5ba7928521ae4af21a55c0e65aa91699
Tamires Sousa Pessoa <109546094+tamirespessoa@users.noreply.github.com>
2024-11-28 12:25:25 -0300

    Create pr.sh

---
+++ pr.sh
@@ -0,0 +1,5 @@
+git add setup_infra.sh
+git commit -m "Primeiro commit - script de infraestrutura"
+git branch -M main
+git remote add origin https://github.com/SEU_USUARIO/infraestrutura-linux.git
+git push -u origin main
